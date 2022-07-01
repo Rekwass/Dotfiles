0f1db8af2e4ba1134d27d4d5db2365abb2ec15d4
Nicolas Moinard <rekwasss@gmail.com>
2022-07-01 11:08:39 +0200

    feat(lsp mappings): Working go to definition and next error

--- nvim/lua/plugin_config/nvim-lspconfig.lua
+++ nvim/lua/plugin_config/nvim-lspconfig.lua
@@ -9,14 +9,77 @@ return function ()
 
   --capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+
+
   local opts = { noremap=true, silent=true }
-  utils.map('n', '<leader>e', ':lua vim.diagnostic.open_float<CR>', opts)
-  utils.map('n', '[d', ':lua vim.diagnostic.goto_prev<CR>', opts)
-  utils.map('n', ']d', ':lua vim.diagnostic.goto_next<CR>', opts)
-  utils.map('n', '<space>q', ':lua vim.diagnostic.setloclist<CR>', opts)
+  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+  local on_attach = function(client, bufnr)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  end
+
+  local servers = {
+    { name = 'clangd' },
+    { name = 'pyright' },
+    { name = 'rust_analyzer' },
+    }
+  
+  for _, server in ipairs(servers) do
+    lspconfig[server.name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end
+
+
+  -- local opts = { noremap=true, silent=true }
+  -- utils.map('n', '<leader>e', ':lua vim.diagnostic.open_float<CR>', opts)
+  -- utils.map('n', '[d', ':lua vim.diagnostic.goto_prev<CR>', opts)
+  -- utils.map('n', ']d', ':lua vim.diagnostic.goto_next<CR>', opts)
+  -- utils.map('n', '<space>q', ':lua vim.diagnostic.setloclist<CR>', opts)
+  --
+  -- local on_attach = function(client, bufnr)
+  --   -- Enable completion triggered by <c-x><c-o>
+  --   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  --
+  --   local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  --   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  --   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  --   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  --   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  --   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  --   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  --   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  --   vim.keymap.set('n', '<space>wl', function()
+  --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  --   end, bufopts)
+  --   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  --   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  --   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  --   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  --   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  -- end
 
   --[[
-  local on_attach = function(ls_on_attach)
+  local on_attach = function()
     return function(client, bufnr)
       local function buf_set_keymap(...) utils.buf_map(bufnr, ...) end
         buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.declaration()<CR>')
@@ -46,19 +109,6 @@ return function ()
     end
   end
   ]]--
-
-  local servers = {
-    { name = 'clangd' },
-    { name = 'pyright' },
-    { name = 'rust_analyzer' },
-    }
-
-  for _, server in ipairs(servers) do
-    lspconfig[server.name].setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-  end
 
   --[[
   vim.lsp.protocol.CompletionItemKind = {
