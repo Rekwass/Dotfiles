44517f5e9e39219f47c2f8d83cc6e04c4e6285ac
Nicolas Moinard <rekwasss@gmail.com>
2022-09-18 20:54:17 +0200

    remove(bindings): Unused bidings

--- nvim/lua/plugin_config/nvim-lspconfig.lua
+++ nvim/lua/plugin_config/nvim-lspconfig.lua
@@ -9,29 +9,20 @@ return function ()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-  map('n', '<space>e', ':lua vim.diagnostic.open_float()<CR>')
+  map('n', '<leader>of', ':lua vim.diagnostic.open_float()<CR>')
   map('n', '[d', ':lua vim.diagnostic.goto_prev()<CR>')
   map('n', ']d', ':lua vim.diagnostic.goto_next()<CR>')
-  map('n', '<space>q', ':lua vim.diagnostic.setloclist()<CR>')
 
   local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) utils.buf_map(bufnr, ...) end
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
     buf_set_keymap('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
     buf_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
-    buf_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
     buf_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
-    buf_set_keymap('n', '<C-k>', ':lua vim.lsp.buf.signature_help()<CR>')
-    buf_set_keymap('n', '<space>wa', ':lua vim.lsp.buf.add_workspace_folder()<CR>')
-    buf_set_keymap('n', '<space>wr', ':lua vim.lsp.buf.remove_workspace_folder()<CR>')
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    buf_set_keymap('n', '<space>D', ':lua vim.lsp.buf.type_definition()<CR>')
-    buf_set_keymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
-    buf_set_keymap('n', '<space>ca', ':lua vim.lsp.buf.code_action()<CR>')
     buf_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
-    buf_set_keymap('n', '<space>f', ':lua vim.lsp.buf.formatting()<CR>')
+    buf_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+    buf_set_keymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>')
+    -- buf_set_keymap('n', '<space>D', ':lua vim.lsp.buf.type_definition()<CR>')
   end
 
   local servers = {
