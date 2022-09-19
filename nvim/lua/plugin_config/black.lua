16222861edd3d733c5aaae32c73be14bde9d2583
Nicolas Moinard <rekwasss@gmail.com>
2022-09-19 09:06:52 +0200

    feat(autocmd): Converted Black vim autocmd to lua

--- nvim/lua/plugin_config/black.lua
+++ nvim/lua/plugin_config/black.lua
@@ -1,9 +1,16 @@
 return function ()
 
-  vim.api.nvim_create_autocmd({"Filetype", "BufWritePre"}, {
-    group = "black_on_save",
-    pattern = "python",
-    command = "Black",
+  vim.api.nvim_create_autocmd('FileType', {
+    desc = 'Format python on write using black',
+
+    pattern = 'python',
+    group = vim.api.nvim_create_augroup('black_on_save', { clear = true }),
+    callback = function (opts)
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = opts.buf,
+        command = 'Black'
+      })
+    end,
   })
 
 end
