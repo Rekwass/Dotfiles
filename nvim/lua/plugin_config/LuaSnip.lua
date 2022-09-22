2bc4a1e1ec56c30e2dbca3adfa271f371e291ad5
Nicolas Moinard <rekwasss@gmail.com>
2022-09-22 19:12:04 +0200

    feat(mappings): <C-n> and <C-p> to go to next or previous snippet choice

--- nvim/lua/plugin_config/LuaSnip.lua
+++ nvim/lua/plugin_config/LuaSnip.lua
@@ -1,5 +1,12 @@
 return function ()
 
+  local map = require("utils").map
+
   require("luasnip.loaders.from_lua").load({paths = "./snippets"})
+
+  map('i', '<C-n>', '<Plug>luasnip-next-choice')
+  map('s', '<C-n>', '<Plug>luasnip-next-choice')
+  map('i', '<C-p>', '<Plug>luasnip-prev-choice')
+  map('i', '<C-p>', '<Plug>luasnip-prev-choice')
 
 end
