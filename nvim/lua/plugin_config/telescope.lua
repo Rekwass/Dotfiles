26857c605e0da5ed1aabd393e48db53d57f90a5c
Nicolas Moinard <rekwasss@gmail.com>
2022-09-22 19:32:30 +0200

    feat(telescope): I can now see hidden files

--- nvim/lua/plugin_config/telescope.lua
+++ nvim/lua/plugin_config/telescope.lua
@@ -21,7 +21,7 @@ return function ()
 
   telescope.load_extension('fzf')
 
-  map('n', '<leader>ff', ':lua require(\'telescope.builtin\').find_files()<CR>')
+  map('n', '<leader>ff', ':lua require(\'telescope.builtin\').find_files({hidden=true})<CR>')
   map('n', '<leader>fg', ':lua require(\'telescope.builtin\').live_grep()<CR>')
   map('n', '<leader>fb', ':lua require(\'telescope.builtin\').buffers()<CR>')
   map('n', '<leader>fh', ':lua require(\'telescope.builtin\').help_tags()<CR>')
