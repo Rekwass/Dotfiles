51821fc49ef6097b33c79b29f0bd699e8d50b784
Nicolas Moinard <rekwasss@gmail.com>
2022-10-24 19:20:22 +0200

    feat(treesitter): Added ensure_installed languages

--- nvim/lua/plugin_config/nvim-treesitter.lua
+++ nvim/lua/plugin_config/nvim-treesitter.lua
@@ -1,6 +1,8 @@
 return function()
 
     require("nvim-treesitter.configs").setup {
+        ensure_installed = { "c", "cpp", "python", "lua" },
+
         highlight = {
             enable = true,
         }
