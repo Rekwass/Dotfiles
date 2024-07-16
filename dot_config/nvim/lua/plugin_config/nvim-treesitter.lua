fd362169a586662be64cf7c573c6fce764848c0b
Nicolas Moinard <rekwasss@gmail.com>
2024-07-16 21:30:41 +0200

    delete(architecture): Old archi

--- dot_config/nvim/lua/plugin_config/nvim-treesitter.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = { "regex", "bash", "c", "cpp", "python", "lua", "vim", "markdown", "markdown_inline" },
-        auto_install = true,
-        highlight = {
-            enable = true,
-        }
-    })
-end
