da284ad04d8b9962586a7e3ab0ae0174026a0c36
Nicolas Moinard <rekwasss@gmail.com>
2024-07-16 11:15:50 +0200

    delete(config.lua): Useless file

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -21,4 +21,5 @@ vim.opt.smartindent = true -- Automatically indents on patterns (e.g.: {, })
 vim.opt.mouse = "" -- Disables mouse because why would you need your mouse when using nvim ?
 vim.opt.ignorecase = true
 
-require("config")
+require("plugins")
+require("keybindings.mappings")
