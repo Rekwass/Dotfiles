88a5ec537a1de7d0640359ee281bd091bc86ddbb
Nicolas Moinard <rekwasss@gmail.com>
2024-09-17 23:14:29 +0200

    feat(haskell-tools): Add keybind to evaluate hs code snippets

--- dot_config/nvim/after/ftplugin/haskell.lua
+++ dot_config/nvim/after/ftplugin/haskell.lua
@@ -6,6 +6,9 @@ local opts = { noremap = true, silent = true, buffer = bufnr, }
 -- Toggle a GHCi repl for the current package
 -- vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
 
+-- Evaluate all code snippets
+vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, opts)
+
 -- Toggle a GHCi repl for the current buffer
 vim.keymap.set('n', '<leader>rf', function()
     ht.repl.toggle(vim.api.nvim_buf_get_name(0))
