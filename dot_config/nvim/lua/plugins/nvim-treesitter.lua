81d770a19fd740865b7801289db4b19b1fc8e279
Nicolas Moinard <rekwasss@gmail.com>
2024-09-17 17:00:05 +0200

    fix(treesitter): Enable highlighting by default

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,22 @@
+-- Using `opts` does not enable highlights by default for some reason.
+-- Even Folke in his LazyNvim configuration uses the `config` field.
+-- https://github.com/nvim-treesitter/nvim-treesitter/wiki/Installation#lazynvim
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "regex", "bash", "c", "cpp", "haskell", "python", "lua", "vim", "markdown", "markdown_inline" },
-        auto_install = true,
-        highlight = {
-            enable = true,
-        },
-        indent = {
-            enable = true,
-        },
-        additional_vim_regex_highlighting = false,
-    },
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "regex", "bash", "c", "cpp", "c_sharp", "haskell", "python", "lua", "vim", "markdown", "markdown_inline" },
+            auto_install = true,
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+            },
+            additional_vim_regex_highlighting = false,
+        })
+    end,
 }
