bf16744ee2044f4758dc22011885f887b6e52fbf
Nicolas Moinard <rekwasss@gmail.com>
2024-09-23 22:27:41 +0200

    feat(nvim-lint): Flake8

---
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,18 @@
+return {
+    "mfussenegger/nvim-lint",
+    config = function()
+        local lint = require("lint");
+
+        lint.linters_by_ft = {
+            python = { "flake8" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+            callback = function()
+                -- try_lint without arguments runs the linters defined in `linters_by_ft`
+                -- for the current filetype
+                lint.try_lint()
+            end,
+        })
+    end,
+}
