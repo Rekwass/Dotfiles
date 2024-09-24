be6d94f0523d665d4fce23c9ad266259002217aa
Nicolas Moinard <rekwasss@gmail.com>
2024-09-24 09:28:28 +0200

    feat(nvim-lint): Add cspell and lint on BufEnter

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -7,11 +7,12 @@ return {
             python = { "flake8" },
         }
 
-        vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+        vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave", "BufEnter" }, {
             callback = function()
                 -- try_lint without arguments runs the linters defined in `linters_by_ft`
                 -- for the current filetype
                 lint.try_lint()
+                lint.try_lint("cspell")
             end,
         })
     end,
