86e092c8f97c32de4a3ffd673d3477f7edb91b20
Nicolas Moinard <rekwasss@gmail.com>
2024-09-24 16:06:46 +0200

    fix(hlslens): Set config to true to call setup

--- dot_config/nvim/lua/plugins/nvim-hlslens.lua
+++ dot_config/nvim/lua/plugins/nvim-hlslens.lua
@@ -1,6 +1,7 @@
 return
 {
     "kevinhwang91/nvim-hlslens",
+    config = true,
     keys = {
         { "n",  "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>", desc = "Go to next search" },
         { "N",  "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>", desc = "Go to previous search" },
