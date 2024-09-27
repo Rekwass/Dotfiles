a17da7acc4ace5310f7ac23d1d6a0a449e799d66
Rekwass <rekwasss@gmail.com>
2024-09-27 18:47:03 +0200

    feat(arkenfox): Enable session restore and preserve cookies on shutdown

--- dot_config/arkenfox/user-overrides.js
+++ dot_config/arkenfox/user-overrides.js
@@ -1,26 +1,29 @@
-// My overrides
-user_pref("browser.startup.page", 1); // Set startup page to home
-user_pref("browser.startup.homepage", "about:home"); // Set home and new tab open Firefox Home
-user_pref("browser.newtabpage.enabled", true); // Set newtab page
+// Arkenfox overrides
+
+// WARNING: The following may prevent websites to load / work normally, try disabling it if anything goes wrong
+// user_pref("privacy.resistFingerprinting", true);
+
+user_pref("browser.startup.page", 3); // 0102 Set startup page to resume previous session
+user_pref("browser.startup.homepage", "about:home"); // 0103 Set home and new tab open Firefox Home
+user_pref("browser.newtabpage.enabled", true); // 0104 Set newtab page
+
+// user_pref("browser.sessionstore.privacy_level", 0); // 1003 Optional to restore cookies/formdata
+
+
+user_pref("browser.contentblocking.category", "strict"); // 2701 Set content blocking to strict
+// INFO: The following preference is required to enable session restore after shutdown
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // 2811 Do not erase history, formdata nor download on shutdown
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false); // 2815 Do not erase cookie nor storage on shutdown
+
+user_pref("privacy.resistFingerprinting", false); // 4501 Disable RFP
+user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 Disable window borders (rounded width / height)
+user_pref("webgl.disabled", false); // 4520 Enable WebGL
+
+user_pref("signon.rememberSignons", false); // 5003 Disable saving passwords
+
+// Miscellaneous
 
 // Translations
 user_pref("browser.translations.enable", false); // Disable website translations
 user_pref("browser.translations.panelShown", false); // Hide translation icon from the search bar
 user_pref("browser.translations.automaticallyPopup", false); // Disable "Do you want to translate this site" Popup
-
-// WARNING: The following may prevent websites to load / work normally, try disabling them if anything goes wrong
-// user_pref("browser.contentblocking.category", "strict");
-// user_pref("privacy.resistFingerprinting", true);
-
-user_pref("privacy.resistFingerprinting", false);
-
-/* override recipe: enable session restore ***/
-user_pref("browser.startup.page", 3); // 0102
-  // user_pref("browser.privatebrowsing.autostart", false); // 0110 required if you had it set as true
-  // user_pref("browser.sessionstore.privacy_level", 0); // 1003 optional to restore cookies/formdata
-user_pref("privacy.clearOnShutdown.history", false); // 2811
-  // user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
-
-user_pref("privacy.resistFingerprinting.letterboxing", false); // Disable window borders (rounded width / height)
-
-user_pref("signon.rememberSignons", false); // 5003: disable saving passwords
