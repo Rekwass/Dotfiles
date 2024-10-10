0af3be4b078f71c2ebddcacb3bb6d61930de5d5d
Rekwass <rekwasss@gmail.com>
2024-10-11 00:46:57 +0200

    doc: Inline comment

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -65,9 +65,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 require("lazy").setup({
     change_detection = {
-        -- automatically check for config file changes and reload the ui
-        enabled = false,
-        notify = false, -- get a notification when changes are found
+        enabled = false, -- automatically check for config file changes and reload the ui
+        notify = false,  -- get a notification when changes are found
     },
     spec = { { import = "plugins" } },
     ui = {
