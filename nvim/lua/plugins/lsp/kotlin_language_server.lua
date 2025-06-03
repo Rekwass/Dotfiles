6b99005ae884613c5d436d897103bcca1741ee92
Rekwass <rekwasss@gmail.com>
2025-06-03 15:42:36 +0200

    feat(lsp): make kotlin_language_server cross platform windows - unix

--- nvim/lua/plugins/lsp/kotlin_language_server.lua
+++ nvim/lua/plugins/lsp/kotlin_language_server.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.configuration = {
+    cmd = { vim.uv.os_uname().version:match("Windows") and 'kotlin-language-server.cmd' or 'kotlin-language-server' },
     capabilities = require('cmp_nvim_lsp').default_capabilities(),
 }
 
